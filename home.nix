0ae9e27abd8de643cb5b8615e5c376b1c7b8e58d
Brian Dawn <brian.t.dawn@gmail.com>
2021-01-30 19:14:07 -0600

    Add more kitty themes.

--- home.nix
+++ home.nix
@@ -25,7 +25,7 @@
     enable = true;
 
     # Update the kitty theme here.
-    settings = import ./kitty-themes/zenburn.nix // {
+    settings = import ./kitty-themes/japanesque.nix // {
       # background_opacity = "0.93";
       font_family = "Jetbrains Mono";
       #bold_font = "FiraCode";

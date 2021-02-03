aff8df451af95e0cfa7a4f4b9e081925a0f3dccb
Brian Dawn <brian.t.dawn@gmail.com>
2021-02-02 18:26:41 -0600

    Use wayland.

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,7 @@
 
   # Enable the GNOME 3 Desktop Environment.
   services.xserver.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
 
@@ -158,6 +159,9 @@
       gnomeExtensions.appindicator
       #gnomeExtensions.no-title-bar
       gnomeExtensions.caffeine
+
+      sway
+      xorg.xeyes
 
       appimage-run
 

4023b66ed9b55e0e176db712307d74560b3e89c1
Brian Dawn <brian.t.dawn@gmail.com>
2021-03-07 18:40:41 -0600

    Add onivim2 in a not ideal way.

--- shared/bash.nix
+++ shared/bash.nix
@@ -10,6 +10,7 @@
       gs = "git status";
       gco = "git checkout";
 
+      oni2-appimage="appimage-run /home/brian/Downloads/Onivim2-x86_64-master.AppImage --force-device-scale-factor 2";
       ssh-kaisa = "ssh 104.131.83.148";
       ssh-kaisa-syncthing = "echo \"visit localhost:8385\" && ssh -L 8385:localhost:8384 104.131.83.148";
       ssh-kilvin="ssh kilvin.farmvision.io";

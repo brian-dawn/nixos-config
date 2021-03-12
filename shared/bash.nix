0711852f8d5a82de5fd8c4addfe6500bc97cde7b
Brian Dawn <brian.t.dawn@gmail.com>
2021-03-12 14:35:37 -0600

    Run formatting.

--- shared/bash.nix
+++ shared/bash.nix
@@ -15,8 +15,8 @@
       # oni2-appimage="appimage-run /home/brian/Downloads/Onivim2-x86_64-master.AppImage --force-device-scale-factor 2";
       ssh-kaisa = "ssh 104.131.83.148";
       ssh-kaisa-syncthing = "echo \"visit localhost:8385\" && ssh -L 8385:localhost:8384 104.131.83.148";
-      ssh-kilvin="ssh kilvin.farmvision.io";
-      ssh-kvothe="ssh kilvin.farmvision.io -p 8004";
+      ssh-kilvin = "ssh kilvin.farmvision.io";
+      ssh-kvothe = "ssh kilvin.farmvision.io -p 8004";
     };
     bashrcExtra = ''
 

211e65518c2a25d872b7d793db51fd0902d82377
Brian Dawn <brian.t.dawn@gmail.com>
2021-04-09 15:09:36 -0500

    Don't use 256 colors for emacs in nox mode.

--- shared/shell-aliases.nix
+++ shared/shell-aliases.nix
@@ -7,6 +7,9 @@
   gs = "git status";
   gco = "git checkout";
 
+  # When emacs is run in nox mode I don't want it to use 256 colors.
+  emacs = "TERM=xterm emacs";
+
   # oni2-appimage="appimage-run /home/brian/Downloads/Onivim2-x86_64-master.AppImage --force-device-scale-factor 2";
   ssh-kaisa = "ssh 104.131.83.148";
   ssh-kaisa-syncthing = "echo \"visit localhost:8385\" && ssh -L 8385:localhost:8384 104.131.83.148";

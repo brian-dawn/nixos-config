630b55929993a66e87ece850f4d29c54536521e6
Brian Dawn <brian.t.dawn@gmail.com>
2021-04-27 20:12:41 -0500

    Handle starship updates.

--- shared/starship.nix
+++ shared/starship.nix
@@ -3,8 +3,6 @@
   programs.starship = {
     enable = true;
     settings.character.success_symbol = "[何](bold green)";
-    settings.enableZshIntegration = true;
-    settings.enableBashIntegration = true;
 
     settings.aws.disabled = true;
     settings.gcloud.disabled = true;

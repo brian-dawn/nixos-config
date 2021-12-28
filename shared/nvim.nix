cf47aaa1f82bd4b2afd56779f774f519e7cf8d92
Brian Dawn <brian.t.dawn@gmail.com>
2021-12-27 19:35:50 -0600

    Autocomplete with tab not enter.

--- shared/nvim.nix
+++ shared/nvim.nix
@@ -94,7 +94,7 @@
               i = cmp.mapping.abort(),
               c = cmp.mapping.close(),
             }),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            ['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           },
           sources = cmp.config.sources({
             { name = 'nvim_lsp' },
